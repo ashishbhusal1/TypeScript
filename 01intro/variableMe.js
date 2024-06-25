6834c3058b13551388dd8f953a1d31709c10126c
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-06-25 20:02:39 +0545

    variables

---
+++ 01intro/variableMe.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Ashish";
+greetings.toLowerCase();
+console.log(greetings);
+//number
+var userId = 3344.5;
+userId.toFixed();
+//userId="ashish"
+//boolean
+var isLOggedIn = false;
+//any
+var hero;
+function getHero() {
+    return "ashish";
+}
+hero = getHero();
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,22 @@
+let greetings: string = "Hello Ashish";
+
+greetings.toLowerCase()
+console.log(greetings);
+
+//number
+let userId = 3344.5
+userId.toFixed()
+//userId="ashish"
+
+//boolean
+let isLOggedIn: boolean = false
+
+//any
+let hero: string;
+function getHero() {
+    return "ashish"
+}
+hero = getHero()
+
+
+export { }
