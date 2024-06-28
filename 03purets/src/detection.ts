803169e5c484b6912ecda77f074bfed9d9e204b3
ashishbhusal1 <ashishbhusal84@gmail.com>
2024-06-28 21:45:45 +0545

    type narrowing

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -69,3 +69,41 @@ function getFood(pet: Fish | Bird) {
         return "Bird food"
     }
 }
+
+
+//discriminated union
+interface Circle {
+    kind: "circle",
+    radius: number
+}
+interface Square {
+    kind: "square",
+    side: number
+}
+interface Rectangle {
+    kind: "rectangle",
+    length: number,
+    width: number
+}
+type Shape = Circle | Square | Rectangle
+
+function getTrueSHape(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2
+    }
+    //return shape.side * shape.side
+}
+
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2
+        case "square":
+            return shape.side * shape.side
+        case "rectangle":
+            return shape.length * shape.width
+        default:
+            const _defaultforshape: never = shape
+            return _defaultforshape
+    }
+}
